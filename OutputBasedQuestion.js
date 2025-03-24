d548a7ced6a787677ffb473892da86f441096d31
ravikant0055 <kumarra927@gmail.com>
2025-03-24 22:28:43 +0530

    Output Question

---
+++ OutputBasedQuestion.js
@@ -0,0 +1,17 @@
+console.log(("6"/"3")==2);   // output: true
+console.log(("6"/"3")===2);  // output: true
+
+console.log(NaN===NaN);     // output: false
+console.log(NaN==NaN);      // output: false
+
+console.log(a);             //output : undefined
+var a = 5;                 
+console.log(a);             //output : 5
+
+console.log(a);             //output : Error
+let a = 5;                 
+console.log(a);            
+
+console.log(1+2+"3");       // output : 33
+console.log("1"+2+3);       // output : 123
+
