ec31a271fabefdeba127ee3d38c0716be12dda23
Ravi Kant <58178306+ravikant0055@users.noreply.github.com>
2025-03-25 21:55:35 +0530

    Update OutputBasedQuestion.js

--- OutputBasedQuestion.js
+++ OutputBasedQuestion.js
@@ -1,8 +1,8 @@
 console.log(("6"/"3")==2);   // output: true
 console.log(("6"/"3")===2);  // output: true
 
-console.log(NaN===NaN);     // output: false
-console.log(NaN==NaN);      // output: false
+console.log(NaN===NaN);     // output: false NaN is not equal to anything even itself 
+console.log(NaN==NaN);      // output: false 
 
 console.log(a);             //output : undefined
 var a = 5;                 
