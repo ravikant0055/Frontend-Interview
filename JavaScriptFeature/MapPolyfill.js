7c81d5c0660b6344efe52de65b23d9a97ed0463c
ravikant0055 <kumarra927@gmail.com>
2025-04-17 20:09:38 +0530

    fix

--- JavaScriptFeature/MapPolyfill.js
+++ JavaScriptFeature/MapPolyfill.js
@@ -2,7 +2,7 @@ const arr=[1,2,3,4,5,6,7,8,9];
 
 Array.prototype.mymap = function(callback, thisArgs){
     let result = [];
-    for(let i=0; i<this.length(); i++){
+    for(let i=0; i<this.length; i++){
         result.push(callback.call(thisArgs,this[i],i));
     }
     return result;
